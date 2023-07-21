b4be488aacdf0a4c966e38ddf376ae0f3d2fd3b5
jhaanamika312 <87661799+jhaanamika312@users.noreply.github.com>
2023-07-21 21:52:06 +0900

    Update app.py

--- sentiment-Analyzer/project-1/app.py
+++ sentiment-Analyzer/project-1/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 import nltk
